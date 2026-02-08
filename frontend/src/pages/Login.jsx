df0eb622d43409785e27493ba0eaa9e9931cb50a
Chanchal Patil <patilchanchal32@gmail.com>
2026-02-08 18:40:42 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     const handleLogin = async () => {
     try {
         const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        `${import.meta.env.VITE_API_URL}/auth/login`,
         form
         );
 
